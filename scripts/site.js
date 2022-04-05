6503664bd7fade254de6503f8c286e71ece55c3c
Amarachi Ajah     -     Software Developer | UX/UI Designer <anitaisbeta@gmail.com>
2022-04-05 14:20:37 +0100

    update

--- scripts/site.js
+++ scripts/site.js
@@ -382,8 +382,7 @@ function generateRickProfile(la) {
 
     var applicationCode = String.raw`\t${createApplicationId()}`;
 
-    var summaryText = foo
-        `Dear ${la.ApplicantName}, <br>
+    var summaryText = foo `Dear ${la.ApplicantName}, <br>
     your application for ${"$" + la.LoanAmount}, ${reviewText}.<br>
     Your risk profile is ${riskProfile}.<br>
     Your unique application code is ${applicationCode}`;
